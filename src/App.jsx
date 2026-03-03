ec36a4d3bed654c189fc0cbc4579a26bb6cd5712
Your Name <your.email@example.com>
2026-03-03 12:10:51 +0000

    Started the CSS for hourly temperatures and worked on css for main weather window
    Co-authored-by: Mohammed Yoonus Ahmed <Yoonus-A@users.noreply.github.com>
    Co-authored-by: M AHMED <ec24373@qmul.ac.uk>

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import './App.css';
 import React from 'react';
 import Background from './Background.jsx';
 import MainWeatherWindow from './MainWeatherWindow.jsx';
+import axios from 'axios';
+import HourInfoPanel from './HourInfoPanel.jsx';
+
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
     <>      
       <Background/>
       <MainWeatherWindow/>
+      <HourInfoPanel/>
     </>
   );
 }
 
 export default App;
+
+
