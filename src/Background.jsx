6f1c023235f091e1683418d70535b1ce05171091
Your Name <your.email@example.com>
2026-03-03 10:57:31 +0000

    Moved functions into dedicated .jsx files and added sunrise icons and tags
    Co-authored-by: Mohammed Yoonus Ahmed <Yoonus-A@users.noreply.github.com>
    Co-authored-by: M AHMED <ec24373@qmul.ac.uk>

---
+++ src/Background.jsx
@@ -0,0 +1,11 @@
+import bg from './Images/background.png';
+
+function Background() {
+  return (
+    <>
+      <img id="background" src={bg} alt="Background" className="background" />
+    </>
+  );
+}
+
+export default Background;
