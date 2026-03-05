e3b70de19eb99039eb22d9855273b197726fc859
Your Name <your.email@example.com>
2026-03-05 15:08:39 +0000

    Main weather window structure amended , app.css changed to be better in terms of display

--- src/HourInfoPanel.jsx
+++ src/HourInfoPanel.jsx
@@ -4,7 +4,7 @@ function HourInfoPanel() {
         <div className="hour-panel">
             <div className="icon-container">
                 <div className="icon-outline">
-
+                    <img src="/public/images/cloud-rain.svg" alt="Icon" />
                 </div>
             </div>
 
@@ -14,13 +14,15 @@ function HourInfoPanel() {
 
             <div className="wind-icon">
                 <div className="wind-outline">
-
+                    <img src="/public/images/wind.svg" alt="" />
                 </div>
             </div>
 
             <div className="wind-speed">14mph</div>
 
-            <div className="highlight-box"></div>
+            <div className="highlight-box">
+                <img src="/public/images/icon.svg" alt="" />
+            </div>
 
             <div className="time">13:00</div>
 
--- src/MainWeatherWindow.jsx
+++ src/MainWeatherWindow.jsx
@@ -1,30 +1,31 @@
 function MainWeatherWindow() {
   return (
-    <>
-      <div className = "main-weather-window">
-        <div className="location">
-          <h1 className="locationTag">London,</h1>
-          <h1 className="greaterLocationTag">England</h1>
-        </div>
-        <div sideWindow>
-          <div className="sideWindowRow">
-            <img className="sunriseIcon" src="/images/sunrise.svg" alt="Sunrise Icon"/>
-            <h2 className="sunriseTag">6:30</h2>
-            <div className = "sunset">
-              <img className="sunsetIcon" src="/images/sunset.svg" alt="Sunset Icon"/>
-              <h2 className="sunsetTag">18:45</h2>
-            </div>
+    <div className="main-weather-window">
 
-          </div>
+      <div className="location">
+        <h1 className="locationTag">London,</h1>
+        <h1 className="greaterLocationTag">England</h1>
+      </div>
+
+
+      <div className="tempRow">
+        <h1 className="tempTag" id="tempVal">5</h1>
+        <h1 className="degreeTag">°C</h1>
+      </div>
+
+
+      <div className="sideWindowRow">
+        <div className="sunrise">
+          <img className="sunriseIcon" src="/images/sunrise.svg" alt="Sunrise Icon"/>
+          <h2 className="sunriseTag">6:30</h2>
         </div>
-        <div className = "degreeGroup">
-          <div className="tempRow">
-            <h1 className="tempTag" id="tempVal">5</h1>
-            <h1 className="degreeTag">°C</h1>
-          </div>        
+        <div className="sunset">
+          <img className="sunsetIcon" src="/images/sunset.svg" alt="Sunset Icon"/>
+          <h2 className="sunsetTag">18:45</h2>
         </div>
       </div>
-    </>
+    </div>
   );
 }
+
 export default MainWeatherWindow;
