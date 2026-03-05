bd9f78abe299e85c0ac7cc1e950312e7f379f4dd
ronakvisram2006 <Ronakv06@outlook.com>
2026-03-05 17:51:24 +0000

    Added array similar to the top bar for the API call to be easier, changed hour function and css to accompany this

--- src/HourInfoPanel.jsx
+++ src/HourInfoPanel.jsx
@@ -1,22 +1,36 @@
+const hourTimes = [
+  { time: '11:00', temp: '3', rain: '20%', wind: '10mph', icon: '/images/icon.svg' },
+  { time: '12:00', temp: '4', rain: '25%', wind: '12mph', icon: '/images/icon.svg' },
+  { time: '13:00', temp: '4', rain: '30%', wind: '14mph', icon: '/images/icon.svg' },
+  { time: '14:00', temp: '5', rain: '20%', wind: '11mph', icon: '/images/icon.svg' },
+  { time: '15:00', temp: '5', rain: '15%', wind: '9mph',  icon: '/images/icon.svg' },
+];
+
 function HourInfoPanel() {
   return (
-    <div className="hour-panel">
-      <div className="time">13:00</div>
-      <img src="/images/icon.svg" alt="Weather Icon" className="weather-icon-center"/>
-      <div className="temp-row">
-        <span className="temperature">4</span>
-        <span className="degree">°C</span>
-      </div>
-      <div className="bottom-row">
-        <div className="rain-info">
-          <img src="/images/cloud-rain.svg" alt="Rain" className="bottom-icon"/>
-          <span className="humidity">30%</span>
+    <div className="hour-panel-row">
+      {hourTimes.map((hour) => (
+        <div key={hour.time} className="hour-panel">
+          <div className="time">{hour.time}</div>
+          <div className="hour-temp-row">
+            <img src={hour.icon} alt="Weather Icon" className="weather-icon-center"/>
+            <div className="hour-temp-num-row">
+              <span className="temperature">{hour.temp}</span>
+              <span className="degree">°C</span>
+            </div>
+          </div>
+          <div className="bottom-row">
+            <div className="rain-info">
+              <img src="/images/cloud-rain.svg" alt="Rain" className="bottom-icon"/>
+              <span className="humidity">{hour.rain}</span>
+            </div>
+            <div className="wind-info">
+              <img src="/images/wind.svg" alt="Wind" className="bottom-icon"/>
+              <span className="wind-speed">{hour.wind}</span>
+            </div>
+          </div>
         </div>
-        <div className="wind-info">
-          <img src="/images/wind.svg" alt="Wind" className="bottom-icon"/>
-          <span className="wind-speed">14mph</span>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
