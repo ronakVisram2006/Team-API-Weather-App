b3e0e9d8220c44372873d7d6af13f6ee78b50150
Your Name <your.email@example.com>
2026-03-05 15:56:38 +0000

    Adjustments to the mainweatherwindow CSS and created dayRow for top panel

---
+++ src/dayRow.jsx
@@ -0,0 +1,14 @@
+function dayRow(){
+    return(
+        <div className="dayRow">
+            <div className="day">Monday</div>
+            <div className="icon">
+                <img src="/public/images/cloud-rain.svg" alt="Icon" />
+            </div>
+            <div className="temp">4°C</div>
+        </div>
+
+    );
+}
+
+    export default dayRow;
