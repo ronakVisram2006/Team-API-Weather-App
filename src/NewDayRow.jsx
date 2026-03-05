16bdfcc91abcbd549956f036eb26ce6f4e076d7d
Your Name <your.email@example.com>
2026-03-05 16:34:50 +0000

    added currentpanel and day panel different when the user clicks on either one

--- src/NewDayRow.jsx
+++ src/NewDayRow.jsx
@@ -1,14 +1,28 @@
 function NewDayRow() {
   return (
-    <div className="active-day-panel">
-      <div className="day-top-row">
-        <span className="day-name">Tues</span>
-        <span className="day-date">4th</span>
+    <div className="new-day-row">
+      <div className="day-panel">
+        <div className="day-top-row">
+          <span className="day-name">Tues</span>
+          <span className="day-date">4th</span>
+        </div>
+        <div className="day-bottom-row">
+          <span className="day-high-temp">8°</span>
+          <img src="/images/icon.svg" alt="Weather Icon" className="day-icon"/>
+          <span className="day-low-temp">3°</span>
+        </div>
       </div>
-      <div className="day-bottom-row">
-        <span className="day-high-temp">8°</span>
-        <img src="/images/icon.svg" alt="Weather Icon" className="day-icon"/>
-        <span className="day-low-temp">3°</span>
+
+      <div className="current-day-panel">
+        <div className="day-top-row">
+          <span className="day-name">Wed</span>
+          <span className="day-date">5th</span>
+        </div>
+        <div className="day-bottom-row">
+          <span className="day-high-temp">6°</span>
+          <img src="/images/icon.svg" alt="Weather Icon" className="day-icon"/>
+          <span className="day-low-temp">4°</span>
+        </div>
       </div>
     </div>
   );
