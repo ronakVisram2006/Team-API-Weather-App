880e00dc0c70be22c744ffc0857365ba870b0021
ronakvisram2006 <Ronakv06@outlook.com>
2026-03-05 16:22:20 +0000

    Added css from figma and rearranged the newdayrow.jsx file

--- src/NewDayRow.jsx
+++ src/NewDayRow.jsx
@@ -1,18 +1,17 @@
-function NewDayRow(){
-    return(
-        <div className="day-panel">
-            <div className="day-name">Tues</div>
-            <div className="day-high-temp">8°</div>
-            <div className="day-low-temp">3°</div>
-            <div className="day-date">4th</div>
-            <div className="day-icon-container">
-                <div className="day-icon-box">
-                    <img src="/images/cloud-rain.svg" alt="Weather Icon" className="day-icon"/>
-                </div>
-            </div>
-        </div>
-
-    );
+function NewDayRow() {
+  return (
+    <div className="active-day-panel">
+      <div className="day-top-row">
+        <span className="day-name">Tues</span>
+        <span className="day-date">4th</span>
+      </div>
+      <div className="day-bottom-row">
+        <span className="day-high-temp">8°</span>
+        <img src="/images/icon.svg" alt="Weather Icon" className="day-icon"/>
+        <span className="day-low-temp">3°</span>
+      </div>
+    </div>
+  );
 }
 
 export default NewDayRow;
