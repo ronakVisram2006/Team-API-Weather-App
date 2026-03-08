e7397d06e319ce04bfb242ce6fe872afe5201788
ronakvisram2006 <Ronakv06@outlook.com>
2026-03-08 15:04:48 +0000

    API calls added to get accurate data for current temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import './reset.css';
 import './App.css';
-import React from 'react';
 import Background from './Background.jsx';
 import MainWeatherWindow from './MainWeatherWindow.jsx';
-import axios from 'axios';
 import HourInfoPanel from './HourInfoPanel.jsx';
 import NewDayRow from './NewDayRow.jsx';
 import SideInfoHikers from './SideInfoHikers.jsx';
@@ -11,15 +9,30 @@ import SideInfoHikers from './SideInfoHikers.jsx';
 import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [query, setQuery] = useState(""); // will be used if for a searchbar
+  const [weather, setWeather] = useState({});
+
+  const getWeatherObj = (city) => {
+    fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&units=metric&appid=7adc71064a0153510e1edd7ee10cea2b`)
+    .then(res => res.json())
+    .then(result => setWeather(result));
+
+  };
+
+  useEffect(() => {
+    getWeatherObj("Sylhet");
+  }, []);
+
   return (
     <>  
       <Background/> 
       <NewDayRow/>
       <div className="weather-layout">
-        <MainWeatherWindow/>
+        <MainWeatherWindow weather={weather}/>
         <SideInfoHikers/>
       </div>
-      <HourInfoPanel/>   
+      <HourInfoPanel weather={weather}/>   
     </>
   );
 }
