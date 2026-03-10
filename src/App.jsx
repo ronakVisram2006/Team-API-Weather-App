db8f4b652003d05ae5245ceeecd416548dfa9641
ronakvisram2006 <Ronakv06@outlook.com>
2026-03-10 00:52:24 +0000

    Added geolocation prompt for precise location data.

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,44 @@ function App() {
     .then(result => setDailyWeather(result));
   }
 
+  const getWeatherByCoords = (lat, lon) => {
+    fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lon}&units=metric&appid=7adc71064a0153510e1edd7ee10cea2b`)
+    .then(res => res.json())
+    .then(result  => setWeather(result));
+  }
+
+  const getDailyWeatherByCoords = (lat, lon) => {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=14&units=metric&appid=7adc71064a0153510e1edd7ee10cea2b`)
+    .then(res => res.json())
+    .then(result => setDailyWeather(result));
+  }
+
+
   useEffect(() => {
-    const city = city_arr[Math.floor(Math.random() * city_arr.length)];
-    getWeatherObj(city);
-    getDailyWeatherObj(city);
-  }, []);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+
+          getWeatherByCoords(lat, lon);
+          getDailyWeatherByCoords(lat, lon);
+        },
+
+        () => {
+          const city = city_arr[Math.floor(Math.random() * city_arr.length)];
+          getWeatherObj(city);
+          getDailyWeatherObj(city);
+        }
+      );
+    }
+    else {
+      const city = city_arr[Math.floor(Math.random() * city_arr.length)];
+      getWeatherObj(city);
+      getDailyWeatherObj(city);
+    }
+  },  []);
 
 
   return (
