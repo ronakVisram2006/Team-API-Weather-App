bdb4ba8410211c09ee3979479834645b6d831cf7
ronakvisram2006 <Ronakv06@outlook.com>
2026-03-13 00:03:28 +0000

    Added method to alter background depending on the API calls weather description as well as the time of the day, started adding the images and changed CSS so they wont stretch

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,10 +1,26 @@
-import bg from './Images/244040.jpg';
+function getTimeOfDay() {
+  const hour = new Date().getHours();
+  if (hour >= 5 && hour < 8) return "sunrise";
+  if (hour >= 8 && hour < 18) return "day";
+  if (hour >= 18 && hour < 21) return "sunset";
+  return "night";
+}
 
-function Background() {
+const weatherBackgrounds = {
+  sunny:   { day: "/images/background/sunnyDayHiker.jpg",   sunrise: "/images/background/sunriseImage.jpg", sunset: "/images/background/trial.jpeg", night: "/images/background/trial.jpeg" },
+  rainy:   { day: "/images/background/rainyDayhiker.jpg",   sunrise: "/images/background/rainySunriseHiker.jpg",   sunset: "/images/background/trial.jpeg",  night: "/images/background/sunnyDayHiker.jpg" },
+  cloudy:  { day: "/images/background/cloudyDayHiker.jpg",  sunrise: "/images/background/cloudySunriseHiker.jpg",  sunset: "/images/background/trial.jpeg", night: "/images/background/trial.jpeg" },
+  stormy:  { day: "/images/background/stormyDayHiker.jpg",  sunrise: "/images/background/stormySunriseHiker.jpg",  sunset: "/images/background/trial.jpeg", night: "/images/background/trial.jpeg" },
+  snowy:   { day: "/images/background/snowyDayHiker.jpg",   sunrise: "/images/background/snowySunriseHiker.jpg",   sunset: "/images/background/trial.jpeg",  night: "/images/background/trial.jpeg" },
+  foggy:   { day: "/images/background/foggyDayHiker.jpg",   sunrise: "/images/background/foggySunriseHiker.jpg",   sunset: "/images/background/trial.jpeg",  night: "/images/background/trial.jpeg" },
+};
+
+function Background({ condition }) {
+  const timeOfDay = getTimeOfDay();
+  const bg = weatherBackgrounds[condition]?.[timeOfDay] ?? "/images/background/sunriseImage.jpg";
+
   return (
-    <>
-      <img id="background" src={bg} alt="Background" className="background" />
-    </>
+    <img id="background" src={bg} alt="Background" className="background" />
   );
 }
 
