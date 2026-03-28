13824b47d6faba7db35d4c23c5d2d4d73eaf9357
ronakvisram2006 <Ronakv06@outlook.com>
2026-03-28 10:13:08 +0000

    Removed unnecessary backgrounds, changed the images to represent the weather more than cinematics

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,18 +1,16 @@
 function getTimeOfDay() {
   const hour = new Date().getHours();
-  if (hour >= 5 && hour < 8) return "sunrise";
-  if (hour >= 8 && hour < 18) return "day";
-  if (hour >= 18 && hour < 21) return "sunset";
+  if (hour >= 5 && hour < 18) return "day";
   return "night";
 }
 
 const weatherBackgrounds = {
-  sunny:   { day: "/images/background/sunnyDayHiker.jpg",   sunrise: "/images/background/sunriseImage.jpg", sunset: "/images/background/rainySunsetHiker.jpg", night: "/images/background/clearNightHiker.jpg" },
-  rainy:   { day: "/images/background/rainyDayhiker.jpg",   sunrise: "/images/background/rainySunriseHiker.jpg",   sunset: "/images/background/rainySunsetHiker.jpg",  night: "/images/background/rainyNightHiker.jpg" },
-  cloudy:  { day: "/images/background/cloudyDayHiker.jpg",  sunrise: "/images/background/cloudySunriseHiker.jpg",  sunset: "/images/background/cloudySunsetHiker.jpg", night: "/images/background/cloudyNightHiker.jpg" },
-  stormy:  { day: "/images/background/stormyDayHiker.jpg",  sunrise: "/images/background/stormySunriseHiker.jpg",  sunset: "/images/background/stormySunsetHiker.jpg", night: "/images/background/stormyNightHiker.jpg" },
-  snowy:   { day: "/images/background/snowyDayHiker.jpg",   sunrise: "/images/background/snowySunriseHiker.jpg",   sunset: "/images/background/snowySunsetHiker.jpg",  night: "/images/background/snowyNightHiker.jpg" },
-  foggy:   { day: "/images/background/foggyDayHiker.jpg",   sunrise: "/images/background/foggySunriseHiker.jpg",   sunset: "/images/background/foggySunsetHiker.jpg",  night: "/images/background/foggyNightHiker.jpg" },
+  sunny:   { day: "/images/background/sunnyDayHiker.jpg", night: "/images/background/clearNightHiker.jpg" },
+  rainy:   { day: "/images/background/rainyDayHiker.jpg",  night: "/images/background/rainyNightHiker.jpg" },
+  cloudy:  { day: "/images/background/cloudyDayHiker.jpg",  night: "/images/background/cloudyNightHiker.jpg" },
+  stormy:  { day: "/images/background/stormyDayHiker.jpg", night: "/images/background/stormyNightHiker.jpg" },
+  snowy:   { day: "/images/background/snowyDayHiker.jpg",night: "/images/background/snowyNightHiker.jpg" },
+  foggy:   { day: "/images/background/foggyDayHiker.jpg", night: "/images/background/foggyNightHiker.jpg" },
 };
 
 function Background({ condition }) {
